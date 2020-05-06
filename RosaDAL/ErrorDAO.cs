9b2df9ba588e147578e2073d28dde7aaf1399fa9
logoffro1 <54850409+logoffro1@users.noreply.github.com>
2020-05-06 17:25:40 +0200

    Add files via upload

---
+++ RosaDAL/ErrorDAO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RosaDAL
+{
+    class ErrorDAO
+    {
+
+        public ErrorDAO(string explanation)
+        {
+            string file = @"../../../Error.log";
+            StreamWriter writer = File.AppendText(file);
+            if (!File.Exists(file))
+            {
+                File.Create(file);
+            }
+            writer.WriteLine("Occurence time: " + DateTime.Now.ToString() + " Explanation : " + explanation);
+            writer.Close();
+            MessageBox.Show(explanation, "Error Occured", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
