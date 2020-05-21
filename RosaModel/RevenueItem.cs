51e0a7caf1d2da3a0dfb0825371abdd80983505d
AizazAhsan <54851282+AizazAhsan@users.noreply.github.com>
2020-05-21 15:38:14 +0200

    Revenue (Turnover)

---
+++ RosaModel/RevenueItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RosaModel
+{
+    public class RevenueItem
+    {
+        public string orderItems;     // name of the item
+        public string menuItems;        
+        public int sales;       // amount of times the item was sold
+        public double cost;     // the price of the item
+        public int boughtByCustomer; // the amount of unique customers it was bought by
+        public double Turnover   // total income from this item 
+        {
+            get
+            {
+                return sales * cost;
+            }
+        }
+    }
+}
