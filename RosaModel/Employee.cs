667a14f8eb026abc6d7bd668778ac5ad4630a987
Liviya Grigorova <657028@student.inholland.nl>
2020-05-28 16:26:44 +0200

    liv tries

--- RosaModel/Employee.cs
+++ RosaModel/Employee.cs
@@ -14,6 +14,7 @@ namespace RosaModel
         public string lastName { get; set; }
         public string username { get; set; } //used for login
         public string password { get; set; } //used for login
+
         public string[] personalNotes { get; set; }
       
     }
